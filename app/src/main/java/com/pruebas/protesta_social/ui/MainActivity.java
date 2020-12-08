ac0fda6bc721da0b6930205c002302e45b044da9
antorresca <65253336+antorresca@users.noreply.github.com>
2020-12-08 14:58:44 -0500

    no se

--- app/src/main/java/com/pruebas/protesta_social/ui/MainActivity.java
+++ app/src/main/java/com/pruebas/protesta_social/ui/MainActivity.java
@@ -1,12 +1,17 @@
 package com.pruebas.protesta_social.ui;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +29,7 @@ import static com.pruebas.protesta_social.ui.Login.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView Imagen;
     private Button btnMapa, btnArengas, btnPanico, btnCalendario,btnChat;
     private Intent intent;
     private DatabaseReference referencia;
@@ -35,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Imagen = (ImageView) findViewById(R.id.Logo_pantalla1);
         btnMapa = (Button) findViewById(R.id.btn_mapa);
         btnArengas = (Button) findViewById(R.id.btn_arengas);
         btnChat = (Button) findViewById(R.id.btnChat);
